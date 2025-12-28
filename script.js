f2a0dbd89821f36d6debf3e80a8f9bec87532a18
Prototypestore <dikeledireeks@outlook.com>
2025-12-28 13:55:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const gradient = document.getElementById("shineGradient");
 let pos = 0;
 
 function animateShine() {
-  pos += 0.025; // slower shimmer for big waves
+  pos += 0.025; // speed of shine (adjustable)
   if (pos > 100) pos = 0;
 
-  // Wider gradient range for huge waves
+  // Wider gradient range for big waves for subtle, dimensional shimmer
   gradient.setAttribute("x1", `${pos - 80}%`);
   gradient.setAttribute("x2", `${pos + 80}%`);
 
